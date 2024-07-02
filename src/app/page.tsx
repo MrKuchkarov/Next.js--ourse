ee37f9adf347710ae1ad1ae9495809eacf323f4f
Mukhammad <yekut1515@yandex.ru>
2024-07-02 15:11:15 +0300

    create: routing links and metadata title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Link from 'next/link'
 
 const Home = () => {
     return (
-        <h1>
-            Home page
-        </h1>
+        <>
+            <h1>Home page</h1>
+            <Link href='/blog'>Blog</Link>
+            <Link href='/products'>Products</Link>
+        </>
     );
 };
 
